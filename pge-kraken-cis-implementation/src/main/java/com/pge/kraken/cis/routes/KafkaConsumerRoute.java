086ed849dcb019517042660e2eb8f30916cc54b3
BHAVESH <saibhavesh.mallidi@abjayon.com>
2026-05-29 17:05:05 +0530

    commit

--- pge-kraken-cis-implementation/src/main/java/com/pge/kraken/cis/routes/KafkaConsumerRoute.java
+++ pge-kraken-cis-implementation/src/main/java/com/pge/kraken/cis/routes/KafkaConsumerRoute.java
@@ -1,5 +1,6 @@
 package com.pge.kraken.cis.routes;
 
+import com.pge.kraken.cis.configs.KafkaPropertiesConfig;
 import com.pge.kraken.cis.logging.StructuredLogger;
 import com.pge.kraken.cis.processors.HeaderEnrichmentProcessor;
 import com.pge.kraken.cis.processors.RequestValidationProcessor;
@@ -11,15 +12,15 @@ import org.slf4j.LoggerFactory;
 public class KafkaConsumerRoute extends RouteBuilder {
 
     private static final Logger LOG = LoggerFactory.getLogger(KafkaConsumerRoute.class);
-    private static final String BROKERS = "localhost:9092";
-    private static final String CONSUMER_TOPIC = "cis.on-demand-read.input";
-    private static final String GROUP_ID = "cis-consumer-group";
 
     @Override
     public void configure() {
+        KafkaPropertiesConfig kafkaConfig = KafkaPropertiesConfig.fromProperties();
+
         LOG.info("Configuring Kafka consumer route");
 
-        fromF("kafka:%s?brokers=%s&groupId=%s&autoOffsetReset=earliest", CONSUMER_TOPIC, BROKERS, GROUP_ID)
+        fromF("kafka:%s?brokers=%s&groupId=%s&autoOffsetReset=earliest",
+                kafkaConfig.getOnDemandReadInputTopic(), kafkaConfig.getBrokers(), kafkaConfig.getConsumerGroupId())
                 .routeId("kafka-consumer-route")
                 .process(exchange -> {
                     LOG.info("Received Kafka message from topic {}", exchange.getIn().getHeader("kafka.TOPIC"));
